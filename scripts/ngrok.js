484eb7bc2b61336830e0f40b486f12d5079e4faa
pBread <4473570+pBread@users.noreply.github.com>
2024-10-24 15:10:00 -0600

    added ngrok for dev

---
+++ scripts/ngrok.js
@@ -0,0 +1,15 @@
+require("dotenv-flow/config");
+
+const hostname = process.env.HOSTNAME;
+const PORT = process.env.PORT || "3000";
+const ngrokArgs = ["http", PORT];
+
+if (hostname) ngrokArgs.push("--url=" + hostname);
+console.log("ngrok " + ngrokArgs.join(" "));
+
+const { spawn } = require("child_process");
+const ngrok = spawn("ngrok", ngrokArgs, { stdio: "inherit" });
+
+ngrok.on("exit", (code) => {
+  console.log(`ngrok process exited with code ${code}`);
+});
