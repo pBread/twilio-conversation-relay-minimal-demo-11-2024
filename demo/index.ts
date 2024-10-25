429f728f195af7d8354c5057dcff99a836c81cac
pBread <4473570+pBread@users.noreply.github.com>
2024-10-25 10:18:18 -0600

    added voice comments

--- demo/index.ts
+++ demo/index.ts
@@ -1,3 +1,9 @@
 export const greeting = `Hello, this is Dave with Copart. Is this Roger?`;
 
+/****************************************************
+ All of these voices: https://www.twilio.com/docs/voice/twiml/say/text-speech#available-voices-and-languages
+ And as of 2024-10-25:
+    Google en-US-Journey-D, en-US-Journey-F, en-US-Journey-O, en-IN-Journey-D, en-IN-Journey-F, en-GB-Journey-D, en-GB-Journey-F, de-DE-Journey-D, de-DE-Journey-F
+    Amazon Amy-Generative, Matthew-Generative, Ruth-Generative
+****************************************************/
 export const tts = { voice: "en-US-Journey-D", provider: "google" };
