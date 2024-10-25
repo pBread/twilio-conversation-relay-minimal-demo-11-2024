e5510fdebf73029a6692d1468e1e51c069c3750f
pBread <4473570+pBread@users.noreply.github.com>
2024-10-24 18:19:42 -0600

    initial openai

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    HOSTNAME: string;
+    OPENAI_API_KEY: string;
+
+    TWILIO_ACCOUNT_SID: string;
+    TWILIO_AUTH_TOKEN: string;
+  }
+}
