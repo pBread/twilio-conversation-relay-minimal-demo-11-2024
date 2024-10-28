3afc5037aff91249898cecec6f61634671f4a649
pBread <4473570+pBread@users.noreply.github.com>
2024-10-28 14:21:03 -0600

    updated openai config

--- demo/index.ts
+++ demo/index.ts
@@ -7,3 +7,5 @@ export const greeting = `Hello, this is Dave with Copart. Is this Roger?`;
     Amazon Amy-Generative, Matthew-Generative, Ruth-Generative
 ****************************************************/
 export const tts = { voice: "en-US-Journey-D", provider: "google" };
+
+export * as openai from "./openai";
---
+++ demo/openai.ts
@@ -0,0 +1,5 @@
+import type { ChatCompletionTool } from "openai/resources";
+
+export const model = "gpt-4-1106-preview";
+
+export const tools: ChatCompletionTool[] = [];
