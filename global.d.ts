5d3c17e8e6b09908da5790fcef7128334cd51264
pBread <4473570+pBread@users.noreply.github.com>
2024-10-30 12:37:46 -0600

    added gemini key

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     HOSTNAME: string;
-    OPENAI_API_KEY: string;
+    GEMINI_API_KEY: string;
 
     TWILIO_ACCOUNT_SID: string;
     TWILIO_AUTH_TOKEN: string;
