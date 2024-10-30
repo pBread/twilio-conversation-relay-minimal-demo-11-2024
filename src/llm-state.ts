776a018f29a2883f5cf889c742e56780655418aa
pBread <4473570+pBread@users.noreply.github.com>
2024-10-29 18:58:40 -0600

    getter

--- src/llm-state.ts
+++ src/llm-state.ts
@@ -16,6 +16,8 @@ export function reset() {
 }
 reset();
 
+export const getMessages = () => [...msgMap.values()];
+
 /****************************************************
  Entities
 ****************************************************/
