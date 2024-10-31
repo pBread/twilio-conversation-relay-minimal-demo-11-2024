cfbf7a4a359b70d1820d01511c965727cbafabee
pBread <4473570+pBread@users.noreply.github.com>
2024-10-31 14:30:50 -0600

    implemented history for startRun in gemini

--- src/gemini.ts
+++ src/gemini.ts
@@ -1,15 +1,20 @@
 import {
+  Content,
+  FunctionCall,
   FunctionDeclaration,
-  FunctionDeclarationSchema,
-  GenerateContentRequest,
+  FunctionResponse,
   GoogleGenerativeAI,
 } from "@google/generative-ai";
 import dotenv from "dotenv-flow";
 import * as demo from "../demo";
-import * as log from "./logger";
 import * as fns from "../demo/functions";
+import * as log from "./logger";
+import * as state from "./state";
 
 dotenv.config();
+
+// only one stream can be open at a time
+let controller: AbortController | undefined;
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 const model = genAI.getGenerativeModel({
@@ -24,13 +29,63 @@ for (const [name, fn] of Object.entries(fns))
     description: fn.description,
   });
 
-export async function startRun() {
-  let params: GenerateContentRequest = {
-    tools: [{ functionDeclarations }],
-    contents: [{ role: "user", parts: [{ text: "What is my " }] }],
-  };
+function translateMessage(msg: state.StoreMessage): Content | undefined {
+  let param: Content | undefined;
 
-  const result = await model.generateContentStream(params);
+  // system messages are sent to Gemini via systemInstructions param.
+  // currently, the last system message overrides the demo.llm.instructions
+  // and all previous system messages
+  if (msg.role === "system") return;
+
+  if (msg.role === "ai" && msg.type === "content") {
+    param = {
+      role: "model",
+      parts: [{ text: msg.content }],
+    };
+  }
+
+  if (msg.role === "ai" && msg.type === "tool") {
+    param = {
+      role: "model",
+      parts: [{ functionCall: JSON.parse(msg.content) as FunctionCall }],
+    };
+  }
+
+  if (msg.role === "human")
+    param = { role: "user", parts: [{ text: msg.content }] };
+
+  if (msg.role === "tool")
+    param = {
+      role: "model",
+      parts: [
+        { functionResponse: JSON.parse(msg.content) as FunctionResponse },
+      ],
+    };
+
+  return param;
+}
+
+export async function startRun() {
+  let systemInstruction: string = demo.llm.instructions;
+
+  let history: Content[] = [];
+  const stateMsgs = state.getMessages();
+  for (const msg of stateMsgs) {
+    if (msg.role === "system") systemInstruction = msg.content;
+    const param = translateMessage(msg);
+    if (param) history.push(param);
+  }
+
+  const message = history.pop();
+  if (!message)
+    throw Error(`Cannot start run because there are no messages in state`);
+
+  const chat = model.startChat({ history, systemInstruction });
+  controller = new AbortController();
+
+  const result = await chat.sendMessageStream(message.parts, {
+    signal: controller.signal,
+  });
 
   // Print text as it comes in.
   for await (const chunk of result.stream) {
@@ -38,4 +93,10 @@ export async function startRun() {
     const chunkText = chunk.text();
     process.stdout.write(chunkText);
   }
+
+  controller = undefined;
+}
+
+export function abort() {
+  return controller?.abort();
 }
